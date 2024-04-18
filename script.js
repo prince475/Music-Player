a81bdbafbb688ed9a5cd8fa9a1155679b51aac60
Prince <43872697+prince475@users.noreply.github.com>
2024-04-18 23:26:58 +0300

    Accessing the #playlist-songs, #play, and #pause elements

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// Accessing the #playlist-songs, #play, and #pause elements with the getElementById() method.
+const playlistSongs = document.getElementById('playlist-songs');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
