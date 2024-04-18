f1f3fd3839acc4177baca5faf8a061b182e3c880
Prince <43872697+prince475@users.noreply.github.com>
2024-04-18 23:31:05 +0300

    Accessing the #next, #previous and #shuffle elements

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 const playlistSongs = document.getElementById('playlist-songs');
 const playButton = document.getElementById('play');
 const pauseButton = document.getElementById('pause');
+
+// Accessing the #next, #previous and #shuffle elements from the HTML file
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
