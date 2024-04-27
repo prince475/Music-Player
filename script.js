55aa48164225b495ec80d8d271c3dcf97cf8906c
Prince <43872697+prince475@users.noreply.github.com>
2024-04-27 22:30:17 +0300

    Adding a return statement that will return a list item for the playlist

--- script.js
+++ script.js
@@ -94,4 +94,6 @@ const renderSongs = (array) => {
   
 }
 
-const songsHTML = array.map((song) => {});
+const songsHTML = array.map((song) => {
+  return `<li id="song-${song.id}" class="playlist-song"></li>`
+});
