5d86d94ba482245e84c96637a68de96e5a513d6b
Prince <43872697+prince475@users.noreply.github.com>
2024-05-11 23:18:43 +0300

    Setting location property for the audio.
    Inside the playSong function, we set the audio.src property equal to song.src. This tells the audio element where to find the audio data for the selected song.
    
    Also, we set the audio.title property equal to song.title. This tells the audio element what to display as the title of the song.

--- script.js
+++ script.js
@@ -93,6 +93,8 @@ let userData = {
 // Implementing the functionality for playing the displayed songs.
 const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id === id);
+    audio.src = song.src
+    audio.title = song.title
 }
 
 const renderSongs = (array) => {
