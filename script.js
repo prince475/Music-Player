56794149eb7e8db2fe879bb3318cd5d81a4d69a1
Prince <43872697+prince475@users.noreply.github.com>
2024-05-14 23:34:41 +0300

    Using the classList property and the add() method to add the "playing" class to the playButton element.
    This will look for the class "playing" in the CSS file and add it to the playButton element.
    
    To finally play the song, use the play() method on the audio variable. play() is a method from the web audio API for playing an mp3 file.

--- script.js
+++ script.js
@@ -101,6 +101,8 @@ const playSong = (id) => {
     audio.currentTime = userData?.songCurrentTime
     }
     userData.currentSong = song
+    playButton.classList.add("playing");
+    audio.play();
 }
 
 const renderSongs = (array) => {
